91f864a82b5df397b0b252f1e0c383759af1a298
Benji Sloan <sloanrobe@hotmail.com>
2024-12-03 12:33:00 +0100

    How to write date objects

---
+++ dates.js
@@ -0,0 +1,8 @@
+const date = new Date(1974, 7, 28, 9, 45)
+console.log(date)
+
+const date2 = new Date('August 28, 1974 09:45:00')
+console.log(date2)
+
+const date3 = new Date(2024, 11, 3, 5, 4, 3)
+console.log(date3)
