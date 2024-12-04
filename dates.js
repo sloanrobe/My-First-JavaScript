c68c1b6301378da824e05f5001e29dcc1a4cac57
Benji Sloan <sloanrobe@hotmail.com>
2024-12-04 13:54:19 +0100

    New examples

--- dates.js
+++ dates.js
@@ -6,3 +6,19 @@ console.log(date2)
 
 const date3 = new Date(2024, 11, 3, 5, 4, 3)
 console.log(date3)
+
+const date1 = new Date()
+
+const year = date.getFullYear()
+const month = date.getMonth()
+const day = date.getDate()
+const dayOfWeek = date.getDay()
+const hour = date.getHours()
+const minute = date.getMinutes()
+
+console.log(year)
+console.log(month)
+console.log(day)
+console.log(dayOfWeek)
+console.log(hour)
+console.log(minute)
