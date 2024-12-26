c01c155e1f7aa1f749344847eab572efe614edbb
sloanrobe <86361390+sloanrobe@users.noreply.github.com>
2024-12-26 16:37:12 +0100

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -4,3 +4,5 @@ When does asynchronous programming / behavior happen?
 What is the purpose of resolving / rejecting
 What is the difference between using promises and the async / await syntax
 What is the NPM and why do we use it?
+
+Why am I writing a question that has nothing to do with this?  Because I am learning about git pull!!
